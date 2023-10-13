44015f07acca5f4a3ace73d05a8f1e1f5840dff1
Himanshu Maharshi <himanshumaharshi45@gmail.com>
2023-10-13 12:07:22 +0530

    dark

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -44,7 +44,7 @@ export default function SignIn() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <h1 className='text-3xl text-center text-white font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='email'
@@ -70,9 +70,9 @@ export default function SignIn() {
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Dont have an account?</p>
+        <p className='text-white'>Dont have an account?</p>
         <Link to={'/sign-up'}>
-          <span className='text-blue-700'>Sign up</span>
+          <span className='text-blue-500'>Sign up</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -41,7 +41,7 @@ export default function SignUp() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center text-white font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
@@ -74,9 +74,9 @@ export default function SignUp() {
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Have an account?</p>
+        <p className='text-white'>Have an account?</p>
         <Link to={'/sign-in'}>
-          <span className='text-blue-700'>Sign in</span>
+          <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
